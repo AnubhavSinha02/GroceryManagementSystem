39bf5a7ad7ed196a69449a8af5de9ecc4625e843
AnubhavSinha02 <sinha.anubhav.as@gmail.com>
2021-01-24 00:59:07 +0530

    Updating code to resolve Cross Origin policy

--- GroceryManagementSystem/src/main/java/com/gms/GroceryManagementSystem/controller/RegistrationController.java
+++ GroceryManagementSystem/src/main/java/com/gms/GroceryManagementSystem/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.gms.GroceryManagementSystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +11,14 @@ import com.gms.GroceryManagementSystem.DAO.User;
 import com.gms.GroceryManagementSystem.service.RegistrationService;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
 public class RegistrationController {
 	
 	@Autowired
 	private RegistrationService service;
 	
 	@PostMapping("/registeruser")
+	@CrossOrigin(origins="http://localhost:4200")
 	public User registerUser(@RequestBody User user) throws Exception{
 		String tempEmailId = user.getEmailId();
 		if(tempEmailId != null && !"".equalsIgnoreCase(tempEmailId)) {
@@ -30,6 +33,7 @@ public class RegistrationController {
 	}
 	
 	@PostMapping("/login")
+	@CrossOrigin(origins="http://localhost:4200")
 	public User loginUser(@RequestBody User user) throws Exception{
 		String tempEmailId = user.getEmailId();
 		String tempPassword = user.getPassword();
