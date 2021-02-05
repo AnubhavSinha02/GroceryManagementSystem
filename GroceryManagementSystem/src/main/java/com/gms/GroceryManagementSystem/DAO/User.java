5caaec077c97198c99d571b15d0d8e39af2adfe6
AnubhavSinha02 <sinha.anubhav.as@gmail.com>
2021-02-06 03:22:55 +0530

    Updating GeneratedValue annotation for Id field in User DAO class

--- GroceryManagementSystem/src/main/java/com/gms/GroceryManagementSystem/DAO/User.java
+++ GroceryManagementSystem/src/main/java/com/gms/GroceryManagementSystem/DAO/User.java
@@ -1,12 +1,15 @@
 package com.gms.GroceryManagementSystem.DAO;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String emailId;
 	private String userName;
